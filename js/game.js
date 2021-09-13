6d9eeed385b6e7b1e9cbdc6140917e34272599aa
ST775 <90621024+ST775@users.noreply.github.com>
2021-09-14 00:34:24 +0900

    NPCのイメージの変更,NPCの台詞の変更,サイト内文章の変更

--- js/game.js
+++ js/game.js
@@ -5,13 +5,18 @@ var img = new Image();
 img.src = "img/pipo-charachip001.png";
 var player = new Character(context, currentMap, 32, 0, 32, 32, 32, img);
 
-var npc = new NPC(context, messageWindowContext, currentMap, 32, 320, 32, 32, 32, img);
-npc.topicRegister(["こんにちは", "私はタカハシです"]);
-npc.topicRegister(["プログラミングに興味があります"]);
+var npcImage1 = new Image();
+npcImage1.src = "img/pipo-charachip002a.png";
 
-var npc2 = new StaticNPC(context, messageWindowContext, currentMap, 32, 160, 32, 32, 32, img);
-npc2.topicRegister(["こんにちは"]);
-npc2.topicRegister(["いい天気ですね"]);
+var npcImage2 = new Image();
+npcImage2.src = "img/pipo-charachip003b.png";
+
+var npc = new NPC(context, messageWindowContext, currentMap, 32, 320, 32, 32, 32, npcImage1);
+npc.topicRegister(["実はこの世界、神が3週間弱で創造したらしいですよ"]);
+
+var npc2 = new StaticNPC(context, messageWindowContext, currentMap, 32, 160, 32, 32, 32, npcImage2);
+npc2.topicRegister(["はじめまして", "web系のプログラミングに興味があります"]);
+npc2.topicRegister(["こちらのサイトはポートフォリオです"]);
 
 var startTime = Date.now();
 window.onload = () => {
@@ -24,6 +29,7 @@ window.onload = () => {
 
     npc2.draw(directions.up);
     npc2.move();
+
 };
 
 document.addEventListener("keydown", (event) => {
